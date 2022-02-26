00204c3adce864bd37b7f974909fd979aa990c01
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-02-26 22:49:31 +0530

    Initial commit

---
+++ app/src/main/java/com/unknowncoder/customprogressbar/MainActivity.java
@@ -0,0 +1,34 @@
+package com.unknowncoder.customprogressbar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button=findViewById(R.id.showDialog);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog alertDialog=new AlertDialog.Builder(getApplicationContext()).create();
+                View view1= LayoutInflater.from(getApplicationContext()).inflate(R.layout.custom_progress_dialog
+                        ,null,false);
+                alertDialog.setView(view1);
+                alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                alertDialog.show();
+            }
+        });
+    }
+}
