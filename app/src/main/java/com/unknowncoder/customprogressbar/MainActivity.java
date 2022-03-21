07940dbd3c1b656bb6015c6ce4f13abc2516fd6f
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-03-21 23:55:12 +0530

    pick the file(pdf,doc,txt,docx)

--- app/src/main/java/com/unknowncoder/customprogressbar/MainActivity.java
+++ app/src/main/java/com/unknowncoder/customprogressbar/MainActivity.java
@@ -1,14 +1,24 @@
 package com.unknowncoder.customprogressbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.jaiselrahman.filepicker.activity.FilePickerActivity;
+import com.jaiselrahman.filepicker.config.Configurations;
+import com.jaiselrahman.filepicker.model.MediaFile;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,13 +32,37 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog alertDialog=new AlertDialog.Builder(MainActivity.this).create();
-                View view1= LayoutInflater.from(getApplicationContext()).inflate(R.layout.custom_progress_dialog
-                        ,null,false);
-                alertDialog.setView(view1);
-                alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                alertDialog.show();
+                Intent intent=new Intent(MainActivity.this, FilePickerActivity.class);
+                intent.putExtra(FilePickerActivity.CONFIGS,new Configurations.Builder()
+                .setCheckPermission(true)
+                .setShowFiles(true)
+                .setShowAudios(false)
+                .setShowImages(false)
+                .setShowVideos(false)
+                .setSuffixes("txt","doc","pdf","docx")
+                .setSkipZeroSizeFiles(true)
+                .build());
+                startActivityForResult(intent,101);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK && data!=null){
+            ArrayList<MediaFile> mediaFiles=data.getParcelableArrayListExtra(FilePickerActivity.MEDIA_FILES);
+            String path=mediaFiles.get(0).getPath();
+            Toast.makeText(MainActivity.this,"Path :- "+path,Toast.LENGTH_SHORT).show();;
+        }
+    }
+
+    private void alertBox(){
+        AlertDialog alertDialog=new AlertDialog.Builder(MainActivity.this).create();
+        View view1= LayoutInflater.from(getApplicationContext()).inflate(R.layout.custom_progress_dialog
+                ,null,false);
+        alertDialog.setView(view1);
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        alertDialog.show();
     }
 }
